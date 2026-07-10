77d1e6e7e1c2215bcddfaa23cc0ae09109ee563c
junbro <wnsgud1235686@gmail.com>
2026-07-10 18:09:08 +0900

    L01_push

---
+++ Lab01_SPI_cam_Integration/SPI_cam_Integration/SPI_cam_Integration.srcs/sources_1/Master_controller.sv
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+
+module Master_controller (
+    input logic clk,
+    input logic reset,
+
+    // img side
+    output logic [clog2(320*240)-1] x_pixel,
+    output logic [clog2(320*240)-1] y_pixel,
+    // SPI side
+    output logic 
+);
+    
+endmodule
